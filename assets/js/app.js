f63c007c00617854d70666f671759382c8345d8e
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2024-12-01 14:59:09 -0800

    v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 feather.replace();
+
+$(function () {
+    $(window).scroll(function () {
+        var scrollPos = $(this).scrollTop()
+
+        if (scrollPos > 0) {
+            $('#navBar').addClass('navbarScrollEffect')
+        } else {
+            $('#navBar').removeClass('navbarScrollEffect')
+        }
+    })
+})
